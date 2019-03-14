84399c6a8417f17b00abacaf4de2eb414f363a6a
ball26810 <ball26810@gmail.com>
2019-03-14 13:59:01 +0800

    layout v2

---
+++ resources/views/account.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Birwin
+ * Date: 2019/3/13
+ * Time: 下午 09:51
+ */
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -6,11 +6,11 @@
     <title>Product Admin - Dashboard HTML Template</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
     <!-- https://fonts.google.com/specimen/Roboto -->
-    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="{{ asset ('css/fontawesome.min.css')}}">
     <!-- https://fontawesome.com/ -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset ('css/bootstrap.min.css')}}">
     <!-- https://getbootstrap.com/ -->
-    <link rel="stylesheet" href="css/templatemo-style.css">
+    <link rel="stylesheet" href="{{asset ('css/templatemo-style.css')}}">
     <!--
 	Product Admin CSS Template
 	https://templatemo.com/tm-524-product-admin
@@ -30,18 +30,12 @@
 <footer class="footer navbar-fixed-bottom">
     @include('layout.footer')
 </footer>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-<script>
-    //easy-sidebar-toggle-right
-    $('.easy-sidebar-toggle').click(function(e) {
-        e.preventDefault();
-//$('body').toggleClass('toggled-right');
-        $('body').toggleClass('toggled');
-//$('.navbar.easy-sidebar-right').removeClass('toggled-right');
-        $('.navbar.easy-sidebar').removeClass('toggled');
-    });
-</script>
+<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+<!-- https://jquery.com/download/ -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<!-- https://getbootstrap.com/ -->
+@section('script')
+    內容~內容~內容
+@show
 </body>
 </html>
