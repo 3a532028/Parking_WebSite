06b7d754b89af66c880b5fa4b56e23ac508b805b
Bruce <s3a532028@student.ncut.edu.tw>
2019-03-16 11:24:32 +0800

    merge

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<div id="myfoot_box3">國立勤益科技大學National Chin-Yi University of Technology<br>
+<div style="background-color: #9fcdff" id="myfoot_box3">國立勤益科技大學National Chin-Yi University of Technology<br>
     地址：<a href="/files/11-1000-24.php" title="交通資訊">41170臺中市太平區坪林里中山路二段 57號 (交通資訊)</a>  | <a href="http://www.ncut.edu.tw/ncut/map/" title="勤益校園地圖">校園地圖</a> <br>
     No.57, Sec. 2, Zhongshan Rd., Taiping Dist., Taichung 41170, Taiwan (R.O.C.)<br>
     電話: <a href="http://www.ncut.edu.tw/ncut/phone/">(04)23924505 (分機表)</a>FAX:(04) 23923363 <a href="http://voip.tanet.edu.tw/sq_in.asp" title="網路電話">TANet 98110000 </a><br>
